e35a7cbd96e74fd97398cc6bb5d44ccb1cd7ebd2
hugo <hugo.varloud@gmail.com>
2024-03-21 23:39:12 +0100

    Navbar menu : Just got it responsive

--- portfolio-hugo-varloud-meyer/src/components/Header.jsx
+++ portfolio-hugo-varloud-meyer/src/components/Header.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
- return (
-   <header className="navigation-menu">
-     <nav>
-      <ul>
-        <li><a href="#section1">Accueil</a></li>
-        <li><a href="#section2">Projets</a></li>
-        <li><a href="#section3">Expérience</a></li>
-        <li><a href="#section4">Contact</a></li>
-      </ul>
-    </nav>
-   </header>
- );
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+  
+  const removeActive = () => {
+    setIsActive(false);
+  }
+  return (
+    <header className="navigation-menu">
+      <nav className="navbar">
+        <div className="container">
+          <div className={`nav-elements  ${showNavbar && 'active'}`}>
+            <ul>
+              <li onClick={removeActive}><a href="#section1">Accueil</a></li>
+              <li onClick={removeActive}><a href="#section2">Projets</a></li>
+              <li onClick={removeActive}><a href="#section3">Expérience</a></li>
+              <li onClick={removeActive}><a href="#section4">Contact</a></li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <MenuIcon />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
