6e6812da8703185a42400950fa387c9f502ca095
hugo <hugo.varloud@gmail.com>
2024-04-16 23:25:40 +0200

    Added the most simple parallax effect on title of first section (wip)

--- portfolio-hugo-varloud-meyer/src/components/Section.jsx
+++ portfolio-hugo-varloud-meyer/src/components/Section.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import "./Sections.css";
 
+
 const Section = ({ id, title }) => {
+  window.addEventListener('scroll', () => {
+    const target = document.querySelector('.section-title');
+    const scrolled = window.scrollY;
+    const rate = scrolled * 0.5;
+    target.style.transform = 'translate3d(0px, ' + rate + 'px, 0px)';
+  })
   return (
-    <section className={id} id={id}>
-      <h2 className='section-title'>{title}</h2>
-    </section>
+    <div className='section-container'>
+      <section className={id} id={id}>
+        <h1 className='section-title'>{title}</h1>
+      </section>
+    </div> 
   );
 };
 
