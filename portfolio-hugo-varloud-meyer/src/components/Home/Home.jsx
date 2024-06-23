16bc6a7708ffbc27c1ae7199a69e66da236646b1
hugo <hugo.varloud@gmail.com>
2024-06-23 15:37:35 +0200

    Curved home section with a bottom left rounded div to display some quote

--- portfolio-hugo-varloud-meyer/src/components/Home/Home.jsx
+++ portfolio-hugo-varloud-meyer/src/components/Home/Home.jsx
@@ -3,10 +3,32 @@ import "./Home.css";
 
 const Home = () => {
   return (
+    <>
     <div className='home-container'>
-      <section className="section1">
-      </section> 
+      <section className="section1"></section> 
     </div>
+      <div className='bottomDiv'>
+        <div className='left'>
+          <div className='curve'></div>
+          <div className='cover'></div>
+          <div className='textPart'>
+            <ul>
+              <li>Look</li>
+              <li>If you had one shot</li>
+              <li>or one opportunity</li>
+              <li>To seize everything you ever wanted</li>
+              <li>in one moment</li>
+              <li>Would you capture it</li>
+              <li>or just let it slip?</li>
+            </ul>
+          </div>
+        </div>
+        <div className='right'>
+          <div className='services'>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
