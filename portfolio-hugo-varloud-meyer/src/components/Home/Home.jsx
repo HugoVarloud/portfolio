25b4a0ac54a57330e1ff69e2fd43f7f2e220d085
hugo <hugo.varloud@gmail.com>
2024-06-30 13:14:27 +0200

    responsive home on all window sizes

--- portfolio-hugo-varloud-meyer/src/components/Home/Home.jsx
+++ portfolio-hugo-varloud-meyer/src/components/Home/Home.jsx
@@ -4,40 +4,40 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-    <div className='home-wrapper'>
-      <div className='home-container'>
-        <section className="section1"></section> 
-      </div>
-
-      <div className='topDiv'>
-        <div className='topLeft'>
-          <div className='topCover'></div>
+      <div className='home-wrapper'>
+        <div className='home-container'>
+          <section className="section1"></section> 
         </div>
-        <div className='topRight'></div>
-      </div>
-      <div className='bottomDiv'>
-        <div className='left'>
-          <div className='curve'></div>
-          <div className='cover'></div>
-          <div className='textPart'>
-            <div class="animated-title">
-              <div class="text-top">
-                <div>
-                  <span>Full Stack Developer</span>
-                  <span>UI/UX Designer</span>
+
+          <div className='topDiv'>
+            <div className='topLeft'>
+              <div className='topCover'></div>
+            </div>
+            <div className='topRight'></div>
+          </div>
+          <div className='bottomDiv'>
+            <div className='left'>
+              <div className='curve'></div>
+              <div className='cover'></div>
+              <div className='textPart'>
+                <div class="animated-title">
+                  <div class="text-top">
+                    <div>
+                      <span>Full Stack Developer</span>
+                      <span>UI/UX Designer</span>
+                    </div>
+                  </div>
+                  <div class="text-bottom">
+                    <div>Contact Me !</div>
+                  </div>
                 </div>
               </div>
-              <div class="text-bottom">
-                <div>Contact Me !</div>
+            </div>
+            <div className='right'>
+              <div className='services'>
               </div>
             </div>
           </div>
-        </div>
-        <div className='right'>
-          <div className='services'>
-          </div>
-        </div>
-      </div>
       </div>
     </>
   );
