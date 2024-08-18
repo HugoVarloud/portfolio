54ac607fb3f277ab18911c4c90a1319e403ddfe3
hugo <hugo.varloud@gmail.com>
2024-08-18 15:50:56 +0200

    removed unused style

--- portfolio-hugo-varloud-meyer/src/components/Experience/Experience.jsx
+++ portfolio-hugo-varloud-meyer/src/components/Experience/Experience.jsx
@@ -5,22 +5,18 @@ import { useLanguageContext } from '../../context/languageContext';
 const projects = [
   {
     title: "ENGIE",
-    description: "2024 - Travail à l'évolution de leur plateforme B2C sur laquelle on retrouve les informations clients et les services et équipement proposé par l'entreprise.",
     src: "/assets/engie-logo.webp",
   },
   {
     title: "SFR",
-    description: "2021 - Développement d'un Saas ayant pour but de gérer les étiquettes et générer les produits, remises et plans de financement des 800 points de vente SFR.",
     src: "/assets/sfr-logo.svg"
   },
   {
     title: "EPITECH",
-    description: "2021 - Diplôme d’Expert en technologies de l’information en ingénierie logicielle enregistré au RNCP de niveau 7.",
     src: "/assets/epitech-logo.jpeg"
   },
   {
     title: "ATIXIS",
-    description: "2020 - Stage de fin d'étude dans une entreprise de pilotage de projet immobillier. Création d'un CRM custom pour répondre aux besoins métier de l'entreprise.",
     src: "/assets/atixis-logo.jpeg"
   }
 ];
