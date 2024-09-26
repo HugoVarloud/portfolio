b3ec28c02e46ea9c4f0ca1b880fff92b43bdb782
hugo <hugo.varloud@gmail.com>
2024-09-26 13:43:14 +0200

    portfolio - feature - modification of the language selector

--- portfolio-hugo-varloud-meyer/src/context/languageContext.jsx
+++ portfolio-hugo-varloud-meyer/src/context/languageContext.jsx
@@ -8,17 +8,21 @@ import './context.css'
 export const LanguageContext = createContext(undefined);
 
 export const LanguageContextProvider = ({ children }) => {
-  const languages = {
-    en: { nativeName: "en" },
-    fr: { nativeName: "fr" },
-  };
+  const languages = [
+    { code: 'en', name: 'English'},
+    { code: 'fr', name: 'French'},
+    { code: 'deu', name: 'Deutsch'}
+  ];
   const { t, i18n } = useTranslation();
   const [currentLanguage, setCurrentLanguage] = useState(i18n.language || "en");
 
   const onClickLanguageChange = (e) => {
-    const language = e.target.value;
-    i18n.changeLanguage(language); //change the language
-    setCurrentLanguage(language); // update state
+    // const language = e.target.value;
+    console.log('------------------> ', e);
+    
+    // i18n.changeLanguage(e);
+    setCurrentLanguage(e);
+    console.log('-----------------XX-> ', currentLanguage);
   };
 
   return (
