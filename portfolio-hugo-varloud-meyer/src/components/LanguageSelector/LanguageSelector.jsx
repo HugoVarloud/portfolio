335e83783aa2b2446b20675160b8ad6915a8e5c6
hugo <hugo.varloud@gmail.com>
2025-09-01 00:16:10 -0500

    Portfolio - aligned language selector

--- portfolio-hugo-varloud-meyer/src/components/LanguageSelector/LanguageSelector.jsx
+++ portfolio-hugo-varloud-meyer/src/components/LanguageSelector/LanguageSelector.jsx
@@ -1,21 +1,26 @@
 import { useLanguageContext } from "../../context/languageContext";
-import "./LanguageSelector.css"
+import "./LanguageSelector.css";
 const LanguageSelector = () => {
-  const { languages, onClickLanguageChange, currentLanguage } = useLanguageContext();  
+  const { languages, onClickLanguageChange, currentLanguage } =
+    useLanguageContext();
   return (
     <>
-      <div className="lang"> 
-        <div className={currentLanguage}>
-        </div>
-          <ul className="dropdown" >
-            {
-              languages.map(language => {
-                if (language.code != currentLanguage) {
-                    return <li key={language.code} onClick={() => onClickLanguageChange(language.code)}><div value={language.code} className={language.code}></div></li>
-                  }
-              })
+      <div className="lang">
+        <div className={currentLanguage}></div>
+        <ul className="dropdown">
+          {languages.map((language) => {
+            if (language.code != currentLanguage) {
+              return (
+                <li
+                  key={language.code}
+                  onClick={() => onClickLanguageChange(language.code)}
+                >
+                  <div value={language.code} className={language.code}></div>
+                </li>
+              );
             }
-          </ul>
+          })}
+        </ul>
       </div>
     </>
   );
